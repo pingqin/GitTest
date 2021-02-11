8ef700e26a9cdc3e2b2c09a1fca676426a0d20bf
PeterQin001 <48634837+PeterQin001@users.noreply.github.com>
2021-02-11 17:10:40 -0500

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_version = ">= 0.12.0"
+}
+provider "aws" {
+  version = ">= 2.28.1"
+  region  = "us-east-1"
+}
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "172.16.0.0/16"
+  tags = {
+    Name = "tf-example"
+  }
+}
