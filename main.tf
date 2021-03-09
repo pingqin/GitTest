9b686458d73944e0136f87cb24ea2069358d4455
PeterQin001 <48634837+PeterQin001@users.noreply.github.com>
2021-03-09 14:33:54 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,30 +1,33 @@
-#############
-#  main.tf
-#############
+#  main.tf:wq!
 provider "aws" {
   region  = "us-east-1"
 }
+
 resource "aws_transfer_user" "transfer_server_user" {
-  count = length(keys(var.transfer_server_user_key))
+  count = length(var.transfer_server_user)
+
   server_id      = var.transfer_server
-  user_name      = element(keys(var.transfer_server_user_key), count.index)
+  user_name      = element(var.transfer_server_user, count.index)
   role           = var.transfer_server_role
   home_directory_type = "LOGICAL"
   home_directory_mappings {
     entry =  "/"
     target = "/${var.bucket_name}"
  }
+
 }
+
 resource "aws_transfer_ssh_key" "transfer_server_ssh_key" {
-    count = length(var.transfer_server_user_key)
-    server_id = var.transfer_server
-    user_name = element(aws_transfer_user.transfer_server_user.*.user_name, count.index)
-    body      = element(values(var.transfer_server_user_key), count.index)
-    depends_on = [aws_transfer_user.transfer_server_user]
+  count = length(var.transfer_server_user)
+
+  server_id = var.transfer_server
+  user_name = element(aws_transfer_user.transfer_server_user.*.user_name, count.index)
+  body      = element(var.transfer_server_ssh_key, count.index)
 }
-##############################
+
+#
 # vars.tf define variables
-##############################
+#
 variable  "transfer_server" {
     description = "Transfer_server"
     type = string
@@ -35,14 +38,21 @@ variable "bucket_name" {
     type = string
     default = "ohiobucket2/folder2"
 }
+
 variable  "transfer_server_role" {
     description = "Transfer_server"
     type = string
     default = "arn:aws:iam::013700525790:role/role_ohb"
+
 }
 
-variable "transfer_server_user_key"  {
-    description = "Transfer_server_user_key"
-    type = map
+variable "transfer_server_user" {
+   description = " A list of users"
+   type = list
 }
 
+variable "transfer_server_ssh_key" {
+   description = " A list of public keys"
+   type = list
+
+}
